acab534415458244392d42f0161fb4417d67a1fc
Martin Le <79704080+le-martin@users.noreply.github.com>
2022-04-25 22:29:40 +0200

    Enabling RPM Fusion repositories

--- post-script.sh
+++ post-script.sh
@@ -4,6 +4,8 @@ sudo dnf -y update
 echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install vim sddm git bspwm dconf-editor kitty rofi polybar sxhkd thunar picom nitrogen fontawesome-fonts fontawesome-fonts-web firefox arandr autorandr meslo-lg-fonts -y
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
@@ -12,7 +14,7 @@ mkdir ~/.config
 cp -r fedora-titus/dotfiles/* ~/.config/
 cp fedora-titus/bg.jpg ~/.config/
 cp fedora-titus/.* ~/
-sudo dnf install ./fedora-titus/rpm-packages/.* -y
+sudo dnf install ~/fedora-titus/rpm-packages/*.rpm -y
 
 echo "Session=bspwm" | sudo tee -a /etc/sddm.conf > /dev/null
 
