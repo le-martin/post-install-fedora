d8de6dfad8e199dad074f17e8ea357b826ce3f80
Martin Le <79704080+le-martin@users.noreply.github.com>
2022-04-25 22:42:27 +0200

    fastestmirror

--- post-script.sh
+++ post-script.sh
@@ -3,7 +3,7 @@
 sudo dnf -y update
 echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
-echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install vim sddm git bspwm dconf-editor kitty rofi polybar sxhkd thunar picom nitrogen fontawesome-fonts fontawesome-fonts-web firefox arandr -y
