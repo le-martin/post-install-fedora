d966f13eab214b6e29a96df691c62b3bfe32bb0d
ichbinram <ramprasadraghunath@gmail.com>
2022-04-25 23:37:52 +0200

    HOME and USER

--- post-script.sh
+++ post-script.sh
@@ -13,13 +13,13 @@ sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 git clone https://github.com/christitustech/fedora-titus
 mkdir -p /home/$USER/.config
-cp -r $dir/fedora-titus/dotconfig/* /home/$HOME/.config/
-cp $dir/fedora-titus/bg.jpg /home/$USER/.config/
-cp $dir/fedora-titus/.* /home/$USER/
+cp -r $dir/fedora-titus/dotconfig/* $HOME/.config/
+cp $dir/fedora-titus/bg.jpg $HOME/.config/
+cp $dir/fedora-titus/.* $HOME/
 sudo dnf install $dir/fedora-titus/rpm-packages/*.rpm -y
 
-sudo cp /home/$USER/post-install-fedora/sddm.conf /etc/sddm.conf
-mkdir -p /home/$USER/.fonts
-cd /home/$USER/.fonts
+sudo cp $HOME/post-install-fedora/sddm.conf /etc/sddm.conf
+mkdir -p $HOME/.fonts
+cd $HOME/.fonts
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
 unzip FiraCode.zip
