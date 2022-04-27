4cdb2f1d00c2359d3f40c130a9471fc380057748
ichbinram <ichbinram@protonmail.com>
2022-04-28 00:05:41 +0200

    awesome fedora

--- post-script.sh
+++ post-script.sh
@@ -3,24 +3,30 @@
 #TODO: Install Meslo-fonts, autorandr
 dir=$(pwd)
 sudo dnf -y update
+# dnf optimizations
 echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+# enable rpm fusion
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install vim sddm git dconf-editor rofi thunar picom nitrogen fontawesome-fonts fontawesome-fonts-web firefox arandr NetworkManager-wifi NetworkManager-tui -y
-sudo dnf install qt qt5-qtquickcontrols2-devel.x86_64 qt5-qtquickcontrols2.x86_64 pip -y
-sudo dnf install lua libtermcap-devel ncurses-devel libevent-devel readline-devel gzip asciidoctor pipewire ffmpeg i3lock zsh -y
+# install dependencies
+sudo dnf install neovim sddm git dconf-editor rofi nautilus picom google-roboto fontawesome-fonts fontawesome-fonts-web firefox arandr NetworkManager-wifi NetworkManager-tui network-manager-applet -y
+sudo dnf install qt qt-qtstyleplugins pip -y
+sudo dnf install pipewire ffmpeg i3lock zsh flameshot polkit-gnome xbacklight xfce4-power-manager lxappearance -y
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+# sddm enable
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
+# copy config files for awesomeWM
+git clone https://github.com/ChrisTitusTech/titus-awesome $HOME/.config/awesome
+# download fedora specific packages
 git clone https://github.com/christitustech/fedora-titus
+# install sddm theme
 git clone https://github.com/keyitdev/sddm-astronaut-theme.git
-git clone https://github.com/awesomeWM/awesome.git
 mkdir -p $HOME/.config
-git clone https://github.com/Mofiqul/awesome-shell.git  $HOME/.config/awesome
 #cp -r $dir/fedora-titus/dotconfig/* $HOME/.config/
 cp $dir/fedora-titus/bg.jpg $HOME/.config/
-cp $dir/fedora-titus/.* $HOME/
 sudo dnf install $dir/fedora-titus/rpm-packages/*.rpm -y
 sudo pip install autorandr
 
@@ -36,7 +42,10 @@ unzip FiraCode.zip
 unzip fad7939b-ubuntu-font-family-0.83.zip
 unzip Ubuntu.zip
 rm -rf *.zip
-
-cd $dir/awesome
-make package
-sudo dnf install *.rpm -y
+# configure rofi
+mkdir -p $HOME/.config/rofi
+cp $HOME/.config/awesome/theme/config.rasi $HOME/.config/rofi/config.rasi
+sed -i '/@import/c\@import "'$HOME'/.config/awesome/theme/sidebar.rasi"' $HOME/.config/rofi/config.rasi
+# parity between qt and gtk applications
+echo "XDG_CURRENT_DESKTOP=Unity" | sudo tee -a /etc/environment > /dev/null
+echo "QT_QPA_PLATFORMTHEME=gtk2" | sudo tee -a /etc/environment > /dev/null
