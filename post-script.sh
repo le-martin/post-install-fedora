9de613337a5ff61c325aef0e759dad1061c948e2
ichbinram <ichbinram@protonmail.com>
2022-04-29 23:40:09 +0200

    automate building awesomewm on fedora

--- post-script.sh
+++ post-script.sh
@@ -11,7 +11,7 @@ echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 # install dependencies
-sudo dnf install awesome alacritty neovim sddm git dconf-editor rofi nautilus picom google-roboto-fonts fontawesome-fonts fontawesome-fonts-web firefox arandr -y
+sudo dnf install alacritty neovim sddm git dconf-editor rofi nautilus picom google-roboto-fonts fontawesome-fonts fontawesome-fonts-web firefox arandr -y
 sudo dnf install NetworkManager-wifi NetworkManager-tui network-manager-applet -y
 sudo dnf install qt5-qtquickcontrols qt5-qtquickcontrols2 qt5-qtstyleplugins pip scrot blueman rofi-themes feh ImageMagick clips-xclips -y
 sudo dnf install pipewire dnf-plugins-core ffmpeg i3lock zsh flameshot polkit-gnome xbacklight xfce4-power-manager lxappearance pavucontrol -y
@@ -20,7 +20,7 @@ wget -qO- https://git.io/papirus-icon-theme-install | sh
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 # copy config files for awesomeWM
-git clone -b awesome4.3-stable https://github.com/HikariKnight/material-awesome.git ~/.config/awesome
+#git clone -b awesome4.3-stable https://github.com/HikariKnight/material-awesome.git ~/.config/awesome
 # git clone https://github.com/lcpz/lain.git $HOME/.config/awesome/lain
 # download fedora specific packages
 git clone https://github.com/christitustech/fedora-titus
@@ -34,7 +34,7 @@ sudo pip install autorandr
 
 sudo cp -fdr sddm-astronaut-theme /usr/share/sddm/themes/
 sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
-sudo cp $HOME/post-install-fedora/sddm.conf /etc/sddm.conf
+#sudo cp $HOME/post-install-fedora/sddm.conf /etc/sddm.conf
 mkdir -p $HOME/.fonts
 cd $HOME/.fonts
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
