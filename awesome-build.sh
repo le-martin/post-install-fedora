af1c67440fdfa1f00b7a9adfb42909b8a160e57d
ichbinram <ichbinram@protonmail.com>
2022-04-29 23:43:34 +0200

    minor fix

--- awesome-build.sh
+++ awesome-build.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 
-sudo dnf install cmake cmake-fedora lua-devel lua-lgi xorg-x11-proto-devel libxcb/devel xcb-util xcb-util-cursor xcb-util-cursor xcb-util-keysyms xcb-util-wm xcb-util-xrm-devel xcb-util-keysyms-devel cairo-devel pango-devel glib-devel libX11-devel libX11-xcb libxkbcommon-x11-devel cairo-gobject gobject-introspection gdk-pixbuf2-devel ImageMagick-devel libxdg-basedir-devel startup-notification-devel libxcb-devel xcb-util-cursor-devel xcb-util-wm-devel -y
+sudo dnf install cmake cmake-fedora lua-devel lua-lgi xorg-x11-proto-devel libxcb-devel xcb-util xcb-util-cursor xcb-util-cursor xcb-util-keysyms xcb-util-wm xcb-util-xrm-devel xcb-util-keysyms-devel cairo-devel pango-devel glib-devel libX11-devel libX11-xcb libxkbcommon-x11-devel cairo-gobject gobject-introspection gdk-pixbuf2-devel ImageMagick-devel libxdg-basedir-devel startup-notification-devel libxcb-devel xcb-util-cursor-devel xcb-util-wm-devel -y
 
 git clone https://github.com/awesomeWM/awesome.git
 cd awesome
